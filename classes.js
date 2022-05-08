966bdfce5a56ca42ccfc08423db79208b01f73f2
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-05-08 14:43:11 +0200

    scope and closures

--- classes.js
+++ classes.js
@@ -53,13 +53,78 @@
 
 // console.log(reversed("webmaster"));
 
-capsLock = (str) => {
-  array1 = str.split(" ");
-  let newarray1 = [];
+// capsLock = (str) => {
+//   array1 = str.split(" ");
+//   let newarray1 = [];
 
-  for (let i = 0; i < array1.length; i++) {
-    newarray1.push(array1[i][0].toUpperCase() + array1[i].slice(1));
+//   for (let i = 0; i < array1.length; i++) {
+//     newarray1.push(array1[i][0].toUpperCase() + array1[i].slice(1));
+//   }
+//   return newarray1.join(" ");
+// };
+// console.log(capsLock("the quick brown fox"));
+
+// let count = "7";
+// function foo() {
+//   // The function scope
+//   let count = 0;
+//   console.log(count); // logs 0
+// }
+// foo();
+// console.log(count); // ReferenceError: count is not defined
+
+// function foo() {
+//   // "foo" function scope
+//   let count = 0;
+//   console.log(count); // logs 0
+// }
+// function bar() {
+//   // "bar" function scope
+//   let count = 1;
+//   console.log(count); // logs 1
+// }
+// foo();
+// bar();
+
+// function outerFunc() {
+//   // the outer scope
+//   let outerVar = "I am outside!";
+//   let innerFunc = () => {
+//     // the inner scope
+//     console.log(outerVar); // => logs "I am outside!"
+//   };
+//   innerFunc();
+// }
+// outerFunc();
+
+// const myGlobal = 0;
+// function func() {
+//   const myVar = 1;
+//   console.log(myGlobal);
+//   function innerOfFunc() {
+//     const myInnerVar = 2;
+//     console.log(myVar, myGlobal);
+//     function innerOfInnerOfFunc() {
+//       console.log(myInnerVar, myVar, myGlobal);
+//     }
+//     innerOfInnerOfFunc();
+//   }
+//   innerOfFunc();
+// }
+// func();
+
+function outerFunc() {
+  let outerVar = "I am outside!";
+  function innerFunc() {
+    console.log(outerVar); // => logs "I am outside!"
   }
-  return newarray1.join(" ");
-};
-console.log(capsLock("the quick brown fox"));
+  // innerFunc()
+  return innerFunc;
+}
+
+function execute() {
+  const myInnerFunc = outerFunc();
+  myInnerFunc();
+  // console.log(myInnerFunc);
+}
+execute();
