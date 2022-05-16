da462d85f666bccabb30274cec15e3246cf7569a
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-05-16 22:32:29 +0200

    more examples

--- try_catch.js
+++ try_catch.js
@@ -7,3 +7,24 @@ try {
 } catch {
   console.log(888888);
 }
+
+// try {
+//   throw new Error("Something went wrong");
+// } catch (error) {
+//   console.log(error);
+// }
+
+// try {
+//   try {
+//     throw new Error("oops");
+//   } catch (ex) {
+//     console.error("inner", ex.message);
+//     throw ex;
+//   } finally {
+//     console.log("finally");
+//   }
+// } catch (e) {
+//   console.error("outer", e.message);
+// } finally {
+//   console.log("FINALLY");
+// }
