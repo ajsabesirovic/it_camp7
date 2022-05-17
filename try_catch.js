1f11032befec3761e8fbea317ae33bd37e51db88
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-05-17 09:55:40 +0200

    setTimeout and setInterval

--- try_catch.js
+++ try_catch.js
@@ -1,12 +1,12 @@
-try {
-  try {
-    console.log(count);
-  } catch (e) {
-    console.log(e.message);
-  }
-} catch {
-  console.log(888888);
-}
+// try {
+//   try {
+//     console.log(count);
+//   } catch (e) {
+//     console.log(e.message);
+//   }
+// } catch {
+//   console.log(888888);
+// }
 
 // try {
 //   throw new Error("Something went wrong");
@@ -28,3 +28,45 @@ try {
 // } finally {
 //   console.log("FINALLY");
 // }
+
+// setInterval(() => {
+//   myFunction("I love You !!!");
+// }, 1000);
+
+// function myFunction(value) {
+//   console.log(value);
+// }
+
+// const calculator = (a, b, func) => {
+//   value = a + b;
+//   func(value);
+// };
+
+// calculator(2, 4, displayValue);
+
+// console.log(4)
+// let value = 7
+// value += 3
+// console.log(value)
+
+// setTimeout(() => {
+//   displayValue("first setTimeout");
+// }, 3000);
+
+// console.log(`1234567890`);
+
+// setTimeout(() => {
+//   displayValue("second setTimeout");
+// }, 2000);
+
+// console.log(6);
+
+const displayValue = (value) => {
+  console.log(value);
+};
+
+setInterval(() => {
+  displayValue("interval");
+}, 1000);
+
+console.log("CLG");
